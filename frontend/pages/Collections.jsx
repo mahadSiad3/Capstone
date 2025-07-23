797b13eb38979b4cecb3f35e52d5a4d0ff28618b
mahadsiad3 <mahadsiad3@gmail.com>
2025-07-23 14:32:12 -0400

    fetch of collecitons is woring

--- frontend/pages/Collections.jsx
+++ frontend/pages/Collections.jsx
@@ -1,7 +1,35 @@
+import { useContext, useEffect,useState } from "react";
+import { UserContext } from "../src/App";
+
 function collections(params) {
+
+    const [allAnime, setAllAnime] = useState([]);
+
+    const {username}= useContext(UserContext)
+
+    useEffect(() => {
+        async function fetchCollection() {
+            try {
+                const response = await fetch(`http://localhost:8080/getcollection/${username}`);
+                const data = await response.json();
+                    console.log(data)
+                setAllAnime(data);
+            } catch (error) {
+                console.error("Failed to fetch collection:", error);
+            }
+        }
+
+        if (username) {
+            fetchCollection();
+        }
+    }, [username]);
+
+    console.log(username)
     
     return(
-        <h1>no collections</h1>
+         <div className="homePage">
+          
+        </div>
     )
 }
 
