a272f6838924c045b148a910c7033f4ff87cbc83
mahadsiad3 <mahadsiad3@gmail.com>
2025-07-23 15:47:50 -0400

    adding filtered anime

--- frontend/pages/Collections.jsx
+++ frontend/pages/Collections.jsx
@@ -1,18 +1,17 @@
-import { useContext, useEffect,useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../src/App";
 
 function collections(params) {
 
     const [allAnime, setAllAnime] = useState([]);
 
-    const {username}= useContext(UserContext)
+    const { username } = useContext(UserContext)
 
     useEffect(() => {
         async function fetchCollection() {
             try {
                 const response = await fetch(`http://localhost:8080/getcollection/${username}`);
                 const data = await response.json();
-                    console.log
                 setAllAnime(data);
             } catch (error) {
                 console.error("Failed to fetch collection:", error);
@@ -25,12 +24,21 @@ function collections(params) {
     }, [username]);
 
     console.log(username)
-    
-    return(
-         <div className="homePage">
-          
+
+    const dropped = allAnime.filter(anime => anime.category === 'Dropped');
+    console.log(dropped)
+    const planToWatch = allAnime.filter(anime => anime.category === 'Plan to Watch');
+    console.log(planToWatch)
+    const watching = allAnime.filter(anime => anime.category === 'Watching');
+    const completed = allAnime.filter(anime => anime.category === 'Completed');
+
+
+    return (
+        <div className="homePage">
+
         </div>
     )
 }
 
 export default collections
+
